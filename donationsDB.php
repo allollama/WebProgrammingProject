935852d0199cddf878ee7195773e663d95d59f68
Stan <stanwhitcomb@outlook.com>
2014-11-25 17:07:16 -0800

    Update donationsDB.php

--- donationsDB.php
+++ donationsDB.php
@@ -10,7 +10,7 @@ mysql_select_db("sdb_swhitcom", $con);
 
 $date = date("m/d/Y h:ia");
 
-$sql="INSERT INTO Donations (Name, Donation, Date)
+$sql="INSERT INTO Donations (Name, Donation, Time)
 VALUES
 ('$_POST[name]','$_POST[donation]','$date')";
 
@@ -22,7 +22,7 @@ echo "<html lang='en'><head><meta charset='utf-8'/><title> Donate </title>
         <link rel='stylesheet' type='text/css' href='myStyles.css'></head>
 	   <body><header><span id='title'>FrugalInnovation<span id='labSpan'>Lab</span></span></br>
         Donations</header><main>";
-echo "Thank you for donating " . $_POST[name];
+echo "Thank you for donating " . $_POST['name'];
 echo "</main><footer><address>Created by <a href='mailto:asehatti@scu.edu'>Ashley Sehatti</a>
         and <a href='mailto:swhitcomb@scu.edu'>Stan Whitcomb</a></address></footer></body></html>";
 
